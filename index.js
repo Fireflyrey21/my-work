1a8005d15f5008772758010fca820d9653f5dff5
tugumeandree <andrewtugume2@gmail.com>
2022-04-04 22:18:39 +0300

    added javascript

--- index.js
+++ index.js
@@ -1,2 +1,40 @@
 //Andrew
 /*andrew*/
+
+//CREATE
+
+function create() {
+
+    var btn = document.createElement("button");
+    btn.innerHTML = "New Button";
+    document.body.appendChild(btn);
+
+}
+
+//READ
+
+var copy = document.getElementById("read"); //button
+var paste =document.getElementById('show'); //paragraph
+
+function read() {
+
+    paste.innerHTML = copy.textContent;
+
+}
+
+//UPDATE
+
+function update() {
+
+
+
+}
+
+
+//DELET
+
+function remove() {
+
+
+
+}
